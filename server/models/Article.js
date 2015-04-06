848a6d4c3793a67de0611b5f52928bc3effe833c
Steven Xu <nonumber1989@gmail.com>
2015-04-06 16:36:52 +0800

    change

--- server/models/Article.js
+++ server/models/Article.js
@@ -2,11 +2,13 @@ var mongoose = require("mongoose");
 var Schema   = mongoose.Schema;
 var Comment = require("./Comment");
 
+//var SongSchema = mongoose.model('Comment').schema;
+
 var ArticleSchema = new Schema({
     title: String,
     slug: String,
     content: String,
     author: String,
-    comments: [Comment.schema]
+    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
 });
 mongoose.model('Article', ArticleSchema);
