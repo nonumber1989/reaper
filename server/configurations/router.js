a15109c1c404cc0234af149403f7ea6129fcca38
nonumber1989 <nonumber1989@gmail.com>
2015-06-09 10:35:06 +0800

    enable cors

--- server/configurations/router.js
+++ server/configurations/router.js
@@ -19,7 +19,8 @@ var restifyHeader = function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     // Set custom headers for CORS
-    res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//    res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
     if (req.method == 'OPTIONS') {
         res.status(200).end();
     } else {
