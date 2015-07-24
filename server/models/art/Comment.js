02f5111bdcc8166c50285d16efd280415777dc6c
Xu, Steven <Steven.Xu@sungard.com>
2015-07-24 16:09:21 +0800

    remove:

--- server/models/art/Comment.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
-
-var CommentSchema = new Schema({
-    text: String,
-    author: String
-});
-mongoose.model('Comment', CommentSchema);
