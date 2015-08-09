19219e393bff53f3ba302338bd1862e377b34c00
Steven Xu <nonumber1989@gmail.com>
2015-08-09 17:50:13 +0800

    use authorization header to enable CROS ,clent side use angular-jwt

--- server/configurations/validateRequest.js
+++ server/configurations/validateRequest.js
@@ -14,9 +14,8 @@ module.exports = function (req, res, next) {
         next();
     } else {
 
-        var token = (req.body && req.body.access_token) || (req.query && req.query.access_token) || req.headers['x-access-token'];
+        var token = (req.body && req.body.access_token) || (req.query && req.query.access_token) || req.headers['x-access-token']||req.headers['authorization'];
         var key = (req.body && req.body.x_key) || (req.query && req.query.x_key) || req.headers['x-key'];
-        console.log(token);
         if (token || key) {
             try {
                 var decoded = jwt.decode(token, configuration.tokenSecret);
