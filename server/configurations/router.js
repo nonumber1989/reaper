21c07b133b4c8842a465bd26ac14584ddf16afd6
unknown <steven.xu@CHNSHAD140023.internal.sungard.corp>
2015-08-14 17:08:18 +0800

    fix

--- server/configurations/router.js
+++ server/configurations/router.js
@@ -14,7 +14,7 @@ fs.readdirSync(controllers_path).forEach(function (file) {
 var server = restify.createServer({
   certificate: fs.readFileSync('C:\\Users\\steven.xu\\bitbucktSpace\\TLS\\ryans-cert.pem'),
   key: fs.readFileSync('C:\\Users\\steven.xu\\bitbucktSpace\\TLS\\ryans-key.pem'),
-  name: 'sevenup',
+  name: 'sevenup'
 });
 server.use(logger('dev'));
 
