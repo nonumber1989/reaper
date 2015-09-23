8b2f9991a95d496978b66861c1e40d7d098403f8
Steven Xu <nonumber1989@gmail.com>
2015-09-23 22:58:54 +0800

    user

--- server/configurations/router.js
+++ server/configurations/router.js
@@ -12,9 +12,9 @@ fs.readdirSync(controllers_path).forEach(function (file) {
 })
 
 var server = restify.createServer({
-  //certificate: fs.readFileSync('./server/TLS/ryans-cert.pem'),
-  //key: fs.readFileSync('./server/TLS/ryans-key.pem'),
-  //name: 'sevenup'
+    //certificate: fs.readFileSync('./server/TLS/ryans-cert.pem'),
+    //key: fs.readFileSync('./server/TLS/ryans-key.pem'),
+    //name: 'sevenup'
 });
 server.use(logger('dev'));
 
@@ -28,16 +28,16 @@ var restifyHeader = function (req, res, next) {
 }
 server.use(restifyHeader);
 //for CORS OPTIONS method filter
-var unknownMethodHandler = function(req, res, next) {
+var unknownMethodHandler = function (req, res, next) {
     if (req.method.toLowerCase() === 'options') {
         console.log('received an options method request');
         if (res.methods.indexOf('OPTIONS') === -1) res.methods.push('OPTIONS');
         res.header('Access-Control-Allow-Credentials', true);
         res.header('Access-Control-Allow-Headers', 'Content-type,Accept,Accept-Version,X-Access-Token,Origin,X-Requested-With,Authorization,Api-Version');
         res.header('Access-Control-Allow-Methods', res.methods.join(','));
-        res.header('Access-Control-Allow-Origin',  req.headers.origin);
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
         return res.send(200);
-    } else{
+    } else {
         return res.send(new restify.MethodNotAllowedError());
     }
 }
@@ -49,6 +49,11 @@ server.use(restify.bodyParser());
 
 // User Start
 server.post('/user/login', auth.login);
+
+server.post("/user", controllers.user.createUser)
+server.put("/user/:id", controllers.user.updateUser)
+server.del("/user/:id", controllers.user.deleteUser)
+server.get("/user/:id", controllers.user.viewUser)
 // User End
 
 // Article Start
--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,13 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
+    id: String,
+    name: String,
     email: String,
-    password: String
+    password: String,
+    avatar: String,
+    alt: String,
+    created: Date,
+    desc: String
 });
 mongoose.model('User', UserSchema);
