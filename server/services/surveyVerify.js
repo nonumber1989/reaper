66714f9b09e17ebc94bb33069f9a69212a9d903b
Steven Xu <nonumber1989@gmail.com>
2016-06-27 23:13:55 +0800

    one commit is still commit ,come on!

---
+++ server/services/surveyVerify.js
@@ -0,0 +1,14 @@
+var _ = require('lodash');
+var verifySurvery = function(questions, originQuestions) {
+	_.map(questions, function(question) {
+		var theOrigin = _.find(originQuestions, function(originQuestion) {
+			return originQuestion.choice === question.choice;
+		});
+		if (question.answer === theOrigin.answer) {
+			question.correct = true;
+		} else {
+			question.correct = false;
+		}
+	});
+};
+
